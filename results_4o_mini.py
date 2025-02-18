348afd6641e8787eca258f1ec2cf5a63e402e164
pcbrom <pcbrom@gmail.com>
2025-02-18 11:16:53 -0300

    update

---
+++ results_4o_mini.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+import openai
+
+# Specify the path to your .env file
+dotenv_path = "/mnt/4d4f90e5-f220-481e-8701-f0a546491c35/arquivos/projetos/.env"
+
+# Load the .env file
+load_dotenv(dotenv_path=dotenv_path)
+
+# Access and store the environment variable
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+
