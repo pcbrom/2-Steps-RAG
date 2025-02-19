b39ba916158915f8cc71bf29d09f41909e6ecd9b
pcbrom <pcbrom@gmail.com>
2025-02-19 17:34:19 -0300

    update

--- populate_chromadb.py
+++ populate_chromadb.py
@@ -1,20 +1,36 @@
-import pandas as pd
+import polars as pl
 import chromadb
-from chromadb.config import Settings
+# from chromadb.config import Settings
 from concurrent.futures import ThreadPoolExecutor
 from tqdm import tqdm
+from sentence_transformers import SentenceTransformer
 
 
 # Import NCM data and concatenate info
-df = pd.read_csv('data/BaseDESC_NCM.csv', sep='\t', encoding='utf-8')
+df = pl.read_csv('data/BaseDESC_NCM.csv', separator='\t', encoding='utf-8', n_threads=18)
 
+# Collapse text
 def process_row(row):
     text = f"NCM: {row['NCM']}, Rótulo: {row['rotulo']}, Item: {row['Item']}, Produto: {row['XPROD']}"
     return text
 
+# Parallel process
 with ThreadPoolExecutor() as executor:
-    documents = list(tqdm(executor.map(process_row, df.to_dict('records')), total=len(df), desc="Processing rows"))
-print(documents[0:10])
+    documents = list(tqdm(executor.map(process_row, df.to_dicts()), total=len(df), desc="Processing rows"))
+
+# Load a pre-trained Portuguese sentence transformer model
+# https://huggingface.co/sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2
+model = SentenceTransformer('sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2')
+embeddings = model.encode(documents[0:5])
+print(embeddings)
+
+
+"""model_name = "paraphrase-multilingual-MiniLM-L12-v2"
+model = SentenceTransformer(model_name)
+
+# Generate embeddings for the documents
+embeddings = model.encode(documents)
+
 
 # Create persistent chromadb
 persist_directory = "chroma_db"
@@ -23,8 +39,7 @@ collection_name = "ncm-all-data"
 collection = client.get_or_create_collection(name=collection_name)
 
 # 
-
-"""collection.add(
+collection.add(
     documents=documents,
     ids=[f"id{i}" for i in range(len(df))]
 )
