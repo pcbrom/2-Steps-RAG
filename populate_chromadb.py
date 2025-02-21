d7a0d51d9aa968484f83b90c1c6b938b80f788d9
pcbrom <pcbrom@gmail.com>
2025-02-20 21:39:10 -0300

    update

--- populate_chromadb.py
+++ populate_chromadb.py
@@ -6,7 +6,9 @@ from sentence_transformers import SentenceTransformer
 import shutil
 
 # Import NCM data and concatenate info
-df = pl.read_csv('data/BaseDESC_NCM.csv', separator='\t', encoding='utf-8', n_threads=18)
+with tqdm(total=100, desc="Loading data") as pbar:
+    df = pl.read_csv('data/BaseDESC_NCM.csv', separator='\t', encoding='utf-8', n_threads=18)
+    pbar.update(100)
 
 # Process rows to create documents and metadata
 def process_row(row):
@@ -59,19 +61,18 @@ collection = client.get_or_create_collection(name=collection_name)
 # Populate chromadb in batches
 batch_size = 41666  # Adjust batch size as needed
 print("Start populating chromadb")
-for i in tqdm(range(0, len(documents), batch_size), desc="Adding batches to ChromaDB"):
-    batch_end = min(i + batch_size, len(documents))
-    batch_documents = documents[i:batch_end]
-    batch_embeddings = embeddings[i:batch_end]
-    batch_metadatas = [sanitize_metadata(metadata) for metadata in metadatas[i:batch_end]]
-    batch_ids = [f"id{j}" for j in range(i, batch_end)]
+with tqdm(total=len(documents), desc="Adding to ChromaDB") as pbar:
+    for i in range(0, len(documents), batch_size):
+        batch_end = min(i + batch_size, len(documents))
+        batch_documents = documents[i:batch_end]
+        batch_embeddings = embeddings[i:batch_end]
+        batch_metadatas = [sanitize_metadata(metadata) for metadata in metadatas[i:batch_end]]
+        batch_ids = [f"id{j}" for j in range(i, batch_end)]
 
-    collection.add(
-        documents=batch_documents,
-        embeddings=batch_embeddings,
-        metadatas=batch_metadatas,
-        ids=batch_ids
-    )
-
-# client.persist() # Persist the data
-# print("ChromaDB persistence completed.")
+        collection.add(
+            documents=batch_documents,
+            embeddings=batch_embeddings,
+            metadatas=batch_metadatas,
+            ids=batch_ids
+        )
+        pbar.update(len(batch_documents))
