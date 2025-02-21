3e1c4457a10a0532dac442c096c5eca79aa3a2ea
pcbrom <pcbrom@gmail.com>
2025-02-20 21:29:19 -0300

    update

--- populate_chromadb.py
+++ populate_chromadb.py
@@ -8,17 +8,35 @@ import shutil
 # Import NCM data and concatenate info
 df = pl.read_csv('data/BaseDESC_NCM.csv', separator='\t', encoding='utf-8', n_threads=18)
 
-# Collapse text
+# Process rows to create documents and metadata
 def process_row(row):
-    text = f"NCM: {row['NCM']}, Rótulo: {row['rotulo']}, Item: {row['Item']}, Produto: {row['XPROD']}"
-    return text
+    document = f"NCM: {row['NCM']}, Rótulo: {row['rotulo']}, Item: {row['Item']}, Produto: {row['XPROD']}"
+    metadata = {
+        "NCM": row['NCM'],
+        "rotulo": row['rotulo'],
+        "Item": row['Item'],
+        "Produto": row['XPROD']
+    }
+    return document, metadata
+
+def sanitize_metadata(metadata):
+    sanitized = {}
+    for key, value in metadata.items():
+        # Ensure keys are strings and values are strings, numbers, or booleans
+        if isinstance(key, str):
+            if isinstance(value, (str, int, float, bool)):
+                sanitized[key] = value
+            else:
+                sanitized[key] = str(value)  # Convert to string if not a basic type
+    return sanitized
 
 # Parallel process
 with ThreadPoolExecutor() as executor:
-    documents = list(tqdm(executor.map(process_row, df.to_dicts()), total=len(df), desc="Processing rows"))
+    results = list(tqdm(executor.map(process_row, df.to_dicts()), total=len(df), desc="Processing rows"))
+
+documents, metadatas = zip(*results)
 
 # Load a pre-trained Portuguese sentence transformer model
-# https://huggingface.co/sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2
 model = SentenceTransformer('sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2')
 embeddings = model.encode(documents)
 
@@ -39,16 +57,21 @@ collection_name = "ncm-all-data"
 collection = client.get_or_create_collection(name=collection_name)
 
 # Populate chromadb in batches
-batch_size = 41666  # Maximum batch size
-print("Start populate chromadb")
+batch_size = 41666  # Adjust batch size as needed
+print("Start populating chromadb")
 for i in tqdm(range(0, len(documents), batch_size), desc="Adding batches to ChromaDB"):
     batch_end = min(i + batch_size, len(documents))
     batch_documents = documents[i:batch_end]
     batch_embeddings = embeddings[i:batch_end]
+    batch_metadatas = [sanitize_metadata(metadata) for metadata in metadatas[i:batch_end]]
     batch_ids = [f"id{j}" for j in range(i, batch_end)]
 
     collection.add(
         documents=batch_documents,
         embeddings=batch_embeddings,
+        metadatas=batch_metadatas,
         ids=batch_ids
     )
+
+# client.persist() # Persist the data
+# print("ChromaDB persistence completed.")
